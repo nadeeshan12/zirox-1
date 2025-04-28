b3fbfa5aff3adaf6bedae750296cbc8f8b2a9e44
Ramazan Bakytuly <kuraikaroma@gmail.com>
2025-04-28 09:52:04 +0500

    fix: cleanup

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,14 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectGroup,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
